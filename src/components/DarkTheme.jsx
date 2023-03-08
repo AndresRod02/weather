dc6c221bc1c2fb25e9eda95e97f5aab75118b3bb
Andrés Rodríguez <artdavid2022@gmail.com>
2023-03-08 18:39:14 -0500

    Cambios finales

--- src/components/DarkTheme.jsx
+++ src/components/DarkTheme.jsx
@@ -9,14 +9,12 @@ const DarkTheme = () => {
             document.body.style.color = 'aliceblue',
             document.getElementsByClassName('card')[0].style.background = 'linear-gradient(#5836B3 0% , #5936B4 9.1%, #362A84 100%)',
             document.getElementsByClassName('card')[0].querySelector('button').style.backgroundColor = '#7D69F1'
-            // document.body.style.backgroundRepeat = 'no-repeat'
         }
         else{
             document.body.style.background = 'radial-gradient(#D5F3FF, var(--first-color))',
             document.body.style.color = 'var(--text-color)',
             document.getElementsByClassName('card')[0].style.background = 'linear-gradient(#e5f2ff97, #d5f3ff94 97.4%)',
             document.getElementsByClassName('card')[0].querySelector('button').style.backgroundColor = 'var(--button-color)'
-            // document.body.style.backgroundRepeat = 'no-repeat'
         }
     }
     return (
