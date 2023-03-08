275ccc97f3e55b065a070770d6bbaee1e02dd750
Andrés Rodríguez <artdavid2022@gmail.com>
2023-03-08 14:38:23 -0500

    Cambio import weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Weather from './components/weather'
+import Weather from './components/Weather'
 import { load } from './components/load'
 import DarkTheme from './components/DarkTheme'
 function App() {
